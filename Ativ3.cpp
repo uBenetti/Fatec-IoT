29153af7cfa3022b8f04ed736b31ca1ca64cdcec
Pedro Benetti <pedrobenetti587@gmail.com>
2026-09-08 11:25:34 -0300

    Ativ3 updated and working correctly

--- Ativ3.cpp
+++ Ativ3.cpp
@@ -1,85 +1,81 @@
-#include <Servo.h> 
+#include <Servo.h>
 
-Servo servo; 
-  
-const int potPin = A0; 
-const int servoPin = 9; 
-  
-const int trigPin = 6; 
-const int echoPin = 7; 
-  
-const int ledVerde = 2; 
-const int ledAmarelo = 3; 
-const int ledVermelho = 4; 
-  
-const int buzzerPin = 5; 
-  
-const int distanciaCritica = 20; 
+Servo servo;
 
-void setup() { 
-  Serial.begin(9600); 
-  
-  servo.attach(servoPin); 
-  servo.write(90); 
-  
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT); 
-  
-  pinMode(ledVerde, OUTPUT); 
-  pinMode(ledAmarelo, OUTPUT); 
-  pinMode(ledVermelho, OUTPUT); 
-  
-  pinMode(buzzerPin, OUTPUT); 
-  
-  digitalWrite(ledVerde, LOW); 
-  digitalWrite(ledAmarelo, LOW); 
-  digitalWrite(ledVermelho, LOW); 
-  noTone(buzzerPin); 
-} 
+const int potPin = A0;
+const int servoPin = 9;
+const int trigPin = 7;
+const int echoPin = 6;
+const int ledVerde = 2;
+const int ledAmarelo = 3;
+const int ledVermelho = 4;
+const int buzzerPin = 5;
+const int distanciaCritica = 20;
 
-void loop() { 
-  int valorPot = analogRead(potPin); 
-  int angulo = map(valorPot, 0, 1023, 0, 180); 
-  servo.write(angulo); 
+void setup() {
+  Serial.begin(9600);
+  servo.attach(servoPin);
+  servo.write(90);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 
-  digitalWrite(trigPin, LOW); 
-  delayMicroseconds(2); 
-  digitalWrite(trigPin, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW); 
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledAmarelo, LOW);
+  digitalWrite(ledVermelho, LOW);
 
-  long duracao = pulseIn(echoPin, HIGH); 
-  int distancia = duracao * 0.034 / 2; 
+  noTone(buzzerPin);
+}
 
-  if (distancia > 50 || distancia == 0) { 
-    digitalWrite(ledVerde, HIGH); 
-    digitalWrite(ledAmarelo, LOW); 
-    digitalWrite(ledVermelho, LOW); 
-    noTone(buzzerPin); 
-  } 
-  else if (distancia > distanciaCritica) { 
-    digitalWrite(ledVerde, LOW); 
-    digitalWrite(ledAmarelo, HIGH); 
-    digitalWrite(ledVermelho, LOW); 
-    noTone(buzzerPin); 
-  } 
-  else { 
-    digitalWrite(ledVerde, LOW); 
-    digitalWrite(ledAmarelo, LOW); 
-    digitalWrite(ledVermelho, HIGH); 
-    
-    tone(buzzerPin, 1000); 
-    delay(100);
+void loop() {
+  int valorPot = analogRead(potPin);
+  int angulo = map(valorPot, 0, 1023, 0, 180);
+
+  servo.write(angulo);
+  digitalWrite(trigPin, LOW);
+
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  long duracao = pulseIn(echoPin, HIGH);
+
+  int distancia = duracao * 0.034 / 2;
+  if (distancia > 50 || distancia == 0) {
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledAmarelo, LOW);
+    digitalWrite(ledVermelho, LOW);
     noTone(buzzerPin);
-  } 
+  }
 
-  Serial.print("Potenciometro: "); 
-  Serial.print(valorPot); 
-  Serial.print(" | Servo: "); 
-  Serial.print(angulo); 
-  Serial.print(" graus | Distancia: "); 
-  Serial.print(distancia); 
-  Serial.println(" cm"); 
+  else if (distancia > distanciaCritica) {
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarelo, HIGH);
+    digitalWrite(ledVermelho, LOW);
+    noTone(buzzerPin);
+  }
 
-  delay(100); 
+  else {
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledAmarelo, LOW);
+    digitalWrite(ledVermelho, HIGH);
+    tone(buzzerPin, 1000);
+  }
+  Serial.print("Potenciometro: ");
+  Serial.print(valorPot);
+  Serial.print(" | Servo: ");
+  Serial.print(angulo);
+  Serial.print(" graus");
+  Serial.print(" | Distancia: ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+
+  delay(100);
 }
